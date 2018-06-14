0259c1e343e83378d0f12e7839890fbb1ac928ca
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-13 19:08:02 -0600

    FizzBuzz answered

--- js/app.js
+++ js/app.js
@@ -19,16 +19,38 @@ console.log("working");
 // }
 
 // excited kitten
- const phrase = ["...human...why you taking pictures of me?...the catnip made me do it", "...why does the red dot always get away..."];
-let randomPhrase = phrase[Math.floor(Math.random() * phrase.length)];
+//  const phrase = ["...human...why you taking pictures of me?...the catnip made me do it", "...why does the red dot always get away..."];
+// let randomPhrase = phrase[Math.floor(Math.random() * phrase.length)];
 
-for (let i = 0; i < 21; i++) {
-	console.log("Love me, pet me! HSSSSSS!");
-	if (i % 2 === 0) {
-		console.log(randomPhrase);
+// for (let i = 0; i < 20; i++) {
+// 	console.log("Love me, pet me! HSSSSSS!");
+// 	if (i % 2 === 0) {
+// 		console.log(randomPhrase);
 
 		
+// 	}
+// }
+
+// fizz buzz
+
+for (let i = 1; i < 101; i++ ) {
+	
+	if (i % 3 === 0 && i % 5 === 0 ) {
+	
+		console.log("FizzBuzz");
+	
+	} else if (i % 3 === 0) {
+		
+		console.log("Fizz");
+	
+	} else if (i % 5 === 0) {
+		
+		console.log("Buzz");
+	
+	} else {
+		console.log(i);
 	}
+
 }
 
 
