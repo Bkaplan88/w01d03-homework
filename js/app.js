153c7f172be199bb9fa7c13bc610ff0869e1bbf1
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-13 23:10:14 -0600

    unable to solve median exercise

--- js/app.js
+++ js/app.js
@@ -3,118 +3,118 @@ console.log("working");
 // easy going
 
 
-// for (let i = 1; i < 21; i++) {
-// 	console.log(i);
-// }
+for (let i = 1; i < 21; i++) {
+	console.log(i);
+}
 
-// get even
+get even
 
 
-// for (let i = 0; i < 201; i++) {
-// 	if (i % 2 === 0) {
-// 		console.log(i)
+for (let i = 0; i < 201; i++) {
+	if (i % 2 === 0) {
+		console.log(i)
 
-// 	}
+	}
 
-// }
+}
 
 // excited kitten
-//  const phrase = ["...human...why you taking pictures of me?...the catnip made me do it", "...why does the red dot always get away..."];
-// let randomPhrase = phrase[Math.floor(Math.random() * phrase.length)];
+ const phrase = ["...human...why you taking pictures of me?...the catnip made me do it", "...why does the red dot always get away..."];
+let randomPhrase = phrase[Math.floor(Math.random() * phrase.length)];
 
-// for (let i = 0; i < 20; i++) {
-// 	console.log("Love me, pet me! HSSSSSS!");
-// 	if (i % 2 === 0) {
-// 		console.log(randomPhrase);
+for (let i = 0; i < 20; i++) {
+	console.log("Love me, pet me! HSSSSSS!");
+	if (i % 2 === 0) {
+		console.log(randomPhrase);
 
 		
-// 	}
-// }
+	}
+}
 
-// fizz buzz
+fizz buzz
 
-// for (let i = 1; i < 101; i++ ) {
+for (let i = 1; i < 101; i++ ) {
 	
-// 	if (i % 3 === 0 && i % 5 === 0 ) {
+	if (i % 3 === 0 && i % 5 === 0 ) {
 	
-// 		console.log("FizzBuzz");
+		console.log("FizzBuzz");
 	
-// 	} else if (i % 3 === 0) {
+	} else if (i % 3 === 0) {
 		
-// 		console.log("Fizz");
+		console.log("Fizz");
 	
-// 	} else if (i % 5 === 0) {
+	} else if (i % 5 === 0) {
 		
-// 		console.log("Buzz");
+		console.log("Buzz");
 	
-// 	} else {
-// 		console.log(i);
-// 	}
+	} else {
+		console.log(i);
+	}
 
-// }
+}
 
 // Getting to know you
 
-// const thom = ["Thom", 1000, "Christchurch"]
-// const karolin = ["Karolin", 16, "New York"]
-// const kristyn = ["Kristyn", 5, "Pittsburgh"]
-// const matt = ["Matt H", 186, "Philadelphia"]
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
 
-// thom[0] = "Gameboy";
-// karolin[1] = 17;
-// matt[2] = "Gotham City";
-// kristyn.pop();
-// kristyn.push("Brooklyn");
+thom[0] = "Gameboy";
+karolin[1] = 17;
+matt[2] = "Gotham City";
+kristyn.pop();
+kristyn.push("Brooklyn");
 
-// console.log(thom);
-// console.log(karolin);
-// console.log(kristyn);
-// console.log(matt);
+console.log(thom);
+console.log(karolin);
+console.log(kristyn);
+console.log(matt);
 
-// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
 
-// for (let i = 0; i < ninjaTurtles.length; i++) {
+for (let i = 0; i < ninjaTurtles.length; i++) {
 	
-// 	console.log(ninjaTurtles[i].toUpperCase());
+	console.log(ninjaTurtles[i].toUpperCase());
 
-// }
+}
 
 // Return of the closets
 
-// const kristynsCloset = [
-//   "left shoe",
-//   "cowboy boots",
-//   "right sock",
-//   "GA hoodie",
-//   "green pants",
-//   "yellow knit hat",
-//   "marshmallow peeps"
-// ];
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
 
-// // Thom's closet is more complicated. Check out this nested data structure!!
-// const thomsCloset = [
-//   [
-//     // These are Thom's shirts
-//     "grey button-up",
-//     "dark grey button-up",
-//     "light blue button-up",
-//     "blue button-up",
-//   ],[
-//     // These are Thom's pants
-//     "grey jeans",
-//     "jeans",
-//     "PJs"
-//   ],[
-//     // Thom's accessories
-//     "wool mittens",
-//     "wool scarf",
-//     "raybans"
-//   ]
-// ];
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
 
-// let kristynsShoe = kristynsCloset.shift();
+let kristynsShoe = kristynsCloset.shift();
 
-// thomsCloset[2].push(kristynsShoe);
+thomsCloset[2].push(kristynsShoe);
 
 // let kristynOutfitArr = [];
 
@@ -140,47 +140,47 @@ console.log("working");
 
 // dirty laundry
 
-// for (let i = 0; i < kristynsCloset.length; i++) {
-// 	console.log(`WHIRR: Now washing ${kristynsCloset[i]}`);
-// }
+for (let i = 0; i < kristynsCloset.length; i++) {
+	console.log(`WHIRR: Now washing ${kristynsCloset[i]}`);
+}
 
-// for (let i = 0; i < thomsCloset[0].length; i++) {
+for (let i = 0; i < thomsCloset[0].length; i++) {
 	
-// 	console.log(thomsCloset[0][i]);
+	console.log(thomsCloset[0][i]);
 	
-// }
+}
 
-// for (let i = 0; i < thomsCloset[1].length; i++) {
+for (let i = 0; i < thomsCloset[1].length; i++) {
 	
-// 	console.log(thomsCloset[1][i]);
+	console.log(thomsCloset[1][i]);
 	
-// }
+}
 
-// for (let i = 0; i < thomsCloset[2].length; i++) {
+for (let i = 0; i < thomsCloset[2].length; i++) {
 	
-// 	console.log(thomsCloset[2][i]);
+	console.log(thomsCloset[2][i]);
 	
-// }
+}
 
 // multiples of 3 and 5
 
-// let sum = 0;
+let sum = 0;
 
-// for (let i = 1000; i > 0; i--) {
-// 	if (i % 3 === 0 || i % 5 === 0) {
-// 		sum += i;
-// 	}
-// }
+for (let i = 1000; i > 0; i--) {
+	if (i % 3 === 0 || i % 5 === 0) {
+		sum += i;
+	}
+}
 
-// console.log(sum);
+console.log(sum);
 
 // triangles
 
-// let argument = 7;
+let argument = 7;
 
-// for (let i = 0; i < 7; i++) {
+for (let i = 0; i < 7; i++) {
 
-// 	console.log(argument += "#");
+	console.log(argument += "#");
 
 // }
 
