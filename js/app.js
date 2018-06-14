d160cc02c56c5b3782ba09815a24d477e01d01eb
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-13 18:57:08 -0600

    excited kittens answered

--- js/app.js
+++ js/app.js
@@ -3,17 +3,33 @@ console.log("working");
 // easy going
 // 1
 
-for (let i = 1; i < 21; i++) {
-	console.log(i);
-}
+// for (let i = 1; i < 21; i++) {
+// 	console.log(i);
+// }
 
 // get even
 // 1
 
-for (let i = 0; i < 201; i++) {
+// for (let i = 0; i < 201; i++) {
+// 	if (i % 2 === 0) {
+// 		console.log(i)
+
+// 	}
+
+// }
+
+// excited kitten
+ const phrase = ["...human...why you taking pictures of me?...the catnip made me do it", "...why does the red dot always get away..."];
+let randomPhrase = phrase[Math.floor(Math.random() * phrase.length)];
+
+for (let i = 0; i < 21; i++) {
+	console.log("Love me, pet me! HSSSSSS!");
 	if (i % 2 === 0) {
-		console.log(i)
+		console.log(randomPhrase);
 
+		
 	}
-
 }
+
+
+
